a9ecd66832ab79d093930cdb1f06e9372dc68d37
Jérémy Doiron <jeremy.doiron.1@gmail.com>
2024-03-12 15:20:21 -0400

    Add config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,40 @@
 module.exports = {
-  extends: ["plugin:astro/recommended", "prettier"],
   overrides: [
     {
       files: ["*.astro"],
       parser: "astro-eslint-parser",
       parserOptions: {
-        parser: "@typescript-eslint/parser",
         extraFileExtensions: [".astro"],
       },
     },
+    {
+      extends: [
+        "plugin:@typescript-eslint/recommended-requiring-type-checking",
+      ],
+      files: ["*.ts", "*.tsx"],
+      parserOptions: {
+        project: "tsconfig.json",
+      },
+    },
   ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: "./tsconfig.json",
+  },
+  plugins: ["@typescript-eslint"],
+  extends: [
+    "plugin:astro/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "prettier",
+  ],
+  rules: {
+    "@typescript-eslint/consistent-type-imports": [
+      "warn",
+      {
+        prefer: "type-imports",
+        fixStyle: "inline-type-imports",
+      },
+    ],
+    "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
+  },
 };
